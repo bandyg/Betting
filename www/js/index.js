44f9c2e1a9caf438fd18c24952c431c8c2656764
bandyg <bandyg@gmail.com>
2017-02-23 07:02:15 +0800

    BettingFE project started.

--- www/js/index.js
+++ www/js/index.js
@@ -16,31 +16,108 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+function () {
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+    var app = {
+        // Application Constructor
+        initialize: function () {
+            this.bindEvents();
+        },
+        // Bind Event Listeners
+        //
+        // Bind any events that are required on startup. Common events are:
+        // 'load', 'deviceready', 'offline', and 'online'.
+        bindEvents: function () {
+            document.addEventListener('deviceready', this.onDeviceReady, false);
+        },
+        // deviceready Event Handler
+        //
+        // The scope of 'this' is the event. In order to call the 'receivedEvent'
+        // function, we must explicitly call 'app.receivedEvent(...);'
+        onDeviceReady: function () {
+            app.receivedEvent('deviceready');
+        },
+        // Update DOM on a Received Event
+        receivedEvent: function (id) {
+            var parentElement = document.getElementById(id);
+            var listeningElement = parentElement.querySelector('.listening');
+            var receivedElement = parentElement.querySelector('.received');
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+            listeningElement.setAttribute('style', 'display:none;');
+            receivedElement.setAttribute('style', 'display:block;');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+            console.log('Received Event: ' + id);
+        }
+    };
 
-        console.log('Received Event: ' + id);
+    //---------------UI part------------------//
+    $("a[data-role=tab]").each(function () {
+        var anchor = $(this);
+        anchor.bind("click", function () {
+            $.mobile.changePage(anchor.attr("href"), {
+                transition: "none",
+                changeHash: false
+            });
+            return false;
+        });
+    });
+
+    $("div[data-role=page]").bind("pagebeforeshow", function (e, data) {
+        $.mobile.silentScroll(0);
+        $.mobile.changePage.defaults.transition = 'slide';
+        //var test = $(this).find("a[href=#"+this.id+"]");
+        //test.html("check");
+    });
+    //---------------UI part------------------//
+
+    //---------------functionality------------------//
+    function MakeRequest() {
+
+        this.requestURL = null;
+        this.responseType = null;
+        this.request = new XMLHttpRequest();
     }
-};
 
-app.initialize();
+    MakeRequest.prototype.setRequestURL = function (requestURL) {
+
+        this.requestURL = requestURL;
+    }
+
+    MakeRequest.prototype.sendRequest = function (responseType) {
+
+        this.request.open('GET', this.requestURL);
+        this.request.responseType = responseType;
+        this.request.send();
+    }
+
+    MakeRequest.prototype.bindOnload = function (responseCallback) {
+
+        this.request.onload = responseCallback;
+    }
+
+    MakeRequest.prototype.getResponseReply = function () {
+
+        return this.request.response;
+    }
+
+
+    var request = new MakeRequest();
+    request.setRequestURL('https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json');
+    request.bindOnload(function () {
+        var JSONReply = request.getResponseReply();
+        var superHeroes;
+        if (typeof JSONReply === 'object') {
+            superHeroes = JSONReply;
+        } else {
+            superHeroes = JSON.parse(JSONReply);
+        }
+        populateHeader(superHeroes);
+        showHeroes(superHeroes)
+
+    });
+    request.sendRequest("json");
+    //---------------functionality------------------//
+}
+()
+
+
